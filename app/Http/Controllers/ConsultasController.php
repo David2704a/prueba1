da34b83f5a6181c3f198d178eb39fd645ede761f
Johan <davidbm270@gmail.com>
2024-02-29 07:50:41 -0500

    Creacion de la vista cups y conexcion a la base de datos

--- app/Http/Controllers/ConsultasController.php
+++ app/Http/Controllers/ConsultasController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ConsultasController extends Controller
 {
-    //
+    public function getCups(){
+        $cups = DB::table('cups')->get();
+
+        return view('cups', compact('cups'));
+    }
 }
