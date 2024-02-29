ece073c9f15e6aa08cd44bc586bdf04f559b2d71
Johan <davidbm270@gmail.com>
2024-02-29 08:09:44 -0500

    Terminado

--- resources/views/cups.blade.php
+++ resources/views/cups.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.1/css/dataTables.dataTables.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 
     <title>Document</title>
 </head>
@@ -12,24 +17,38 @@
 
     <h1>CUPS</h1>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Codigo</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($cups as $cup)
+
+    <div
+        class="table-responsive"
+    >
+        <table id="tableCups"
+            class="table table-primary"
+        >
+            <thead>
+                <tr>
+                    <th scope="col">Column 1</th>
+                    <th scope="col">Column 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($cups as $cup)
                 <tr>
                     <td>
                         {{ $cup->codigo_cups}}
                     </td>
+                    <td>
+                        {{ $cup->descripcion}}
+                    </td>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+
+    <script>
+        let table = new DataTable('#tableCups');
+    </script>
 
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 </html>
